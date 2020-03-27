b58e1b50323f8620ad735943babfbce6d608754c
Bezzubikambitious <61292027+Bezzubikambitious@users.noreply.github.com>
2020-03-27 16:07:24 +0200

    Tania

--- set.h
+++ set.h
@@ -11,15 +11,26 @@ class Set
     int *n;
     int Size;
 public:
+    Set();
     Set(int size);
+    Set(int *array, int size );
     Set(const Set &other);//copy constructor
     ~Set();
 
     bool check_in(int x);//check for availability
+    int force();//potyzh
     void add(int x);//adding
     void del(int x);//deleting
     void max();//maximum
     void min();//minimum
+
+    int GetLength();
+    void SetLength();
+    int* Get_Set();
+
+    Set operator+(Set m1);
+    Set operator-(Set m1);
+    int operator==(Set m1);
 
     void SetM1(int *m1);//set1
     void SetM2(int *m2);//set2
